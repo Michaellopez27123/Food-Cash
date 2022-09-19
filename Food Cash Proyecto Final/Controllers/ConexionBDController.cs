6927e7252c8b06acb973472181efa1cb3c3d0fbe
David Muñoz <bdmunozv@gmail.com>
2022-09-19 17:05:38 -0500

    Ingreso Models

---
+++ Food Cash Proyecto Final/Controllers/ConexionBDController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Food_Cash_Proyecto_Final.Controllers
+{
+    public class ConexionBDController : Controller
+    {
+        // GET: ConecxionDB
+        public ActionResult Login()
+        {
+         
+
+
+            return View();
+        }
+    }
+}
---
+++ Food Cash Proyecto Final/Controllers/UsuariosController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Food_Cash_Proyecto_Final.Models;
+
+namespace Food_Cash_Proyecto_Final.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private FoodCashEntities db = new FoodCashEntities();
+
+        // GET: Usuarios
+        public ActionResult Login()
+        {
+            var usuario = db.Usuario.Include(u => u.Persona);
+            return View(usuario.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult BuscarDB(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        public ActionResult ValidarUsuario(long? id, string contrasena)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.FirstOrDefault(e => e.id_usuario == id && e.contrasena == contrasena);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_usuario = new SelectList(db.Persona, "identificacion", "t_documento");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_usuario,contrasena,estado")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_usuario = new SelectList(db.Persona, "identificacion", "t_documento", usuario.id_usuario);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_usuario = new SelectList(db.Persona, "identificacion", "t_documento", usuario.id_usuario);
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_usuario,contrasena,estado")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_usuario = new SelectList(db.Persona, "identificacion", "t_documento", usuario.id_usuario);
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
