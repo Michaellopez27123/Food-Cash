381e4c246107d5b1c85dc27db6a10c24c410960c
Michael Andres Lopez Cardenas <michael.lopez@pi.edu.co>
2022-09-18 20:04:02 -0500

    Ultimo avance 19/09/2022

---
+++ Food Cash Proyecto Final/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Food_Cash_Proyecto_Final.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Food Cash Proyecto Final/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Food_Cash_Proyecto_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Sedes()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+
+    }
+}
